9bd7a8ffab354454d1dba9a6bc46019bf7c5aa0f
AntoineLemlin <ant.lemlin@gmail.com>
2021-09-28 12:29:09 +0200

    fonts

--- wordpress/wp-content/themes/PIL_Project/functions.php
+++ wordpress/wp-content/themes/PIL_Project/functions.php
@@ -2,7 +2,8 @@
 
 function theme_functions() {
     
-    add_theme_support( 'title-tag' );
+    add_theme_support( 'title-tag');
+    add_theme_support('menus');
     
 }
 
@@ -11,6 +12,7 @@ add_action( 'after_setup_theme', 'theme_functions' );
 
 function add_scripts(){
 wp_enqueue_style('style', get_template_directory_uri() . '/assets/styles/display.css', NULL, microtime(), all);
+wp_enqueue_style('fonts', get_template_directory_uri() . '/assets/fonts/II Vorkurs/stylesheet.css', NULL, microtime(), all);
 wp_enqueue_script('script', get_template_directory_uri() . '/JS/main.js',NULL, microtime(), true);
 }
 
---
+++ wordpress/wp-content/themes/PIL_Project/template_parts/sidebar.php
@@ -0,0 +1,12 @@
+<?php /*Template Name: social */ ?>
+
+<aside>
+    <nav>
+        <ul>
+            <li>Fb</li>
+            <li>Tw</li>
+            <li>Lk</li>
+            <li>In</li>
+        </ul>
+    </nav>
+</aside>
