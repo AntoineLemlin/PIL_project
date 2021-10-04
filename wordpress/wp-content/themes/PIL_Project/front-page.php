5f15a4ba5cd4d730d4476ea91e29f77186ecbbdf
AntoineLemlin <ant.lemlin@gmail.com>
2021-10-04 12:00:36 +0200

    design frontpage desktop

--- wordpress/wp-content/themes/PIL_Project/front-page.php
+++ wordpress/wp-content/themes/PIL_Project/front-page.php
@@ -3,7 +3,7 @@
 <?php get_template_part('./template_parts/sidebar'); ?>
 <section id="section-intro">
     <h1>Espaces pour industries créatives</h1>
-    <div class="arrow"><img src="<?php bloginfo('template_url')?>/assets/img/svg/arrow-down.svg" alt=""></svg></div>
+    <div class="arrow"><a href="#projet"><img src="<?php bloginfo('template_url')?>/assets/img/svg/arrow-down.svg" alt=""></a></div>
     <img id="front-page-video" src="<?php bloginfo('template_url'); ?>/assets/img/front-page-image.jpg" alt="front image">
 </section>
 
@@ -31,7 +31,7 @@
     <h2>30+ entreprises / 400+ personnes / 35</h2>
 </div>
 
-<section class="projet">
+<section id="projet" class="projet">
     <h2>Vous avez un projet <span>audiovisuel ou digital ?</span></h2>
 
     <div class="container">
@@ -67,5 +67,53 @@
 
     </div>
 </section>
+
+    <section class="formations">
+        <div class="container">
+            <img src="<?php bloginfo('template_url'); ?>/assets/img/formations-photo.jpg" alt="formations photo">
+            <div class="texte-formations">
+                <h2>Formations audiovisuel</h2>
+                <h4>Vous souhaitez vous former à un métier ou une compétence en pénurie en Wallonie ?</h4>
+                <p>Rendez-vous sur le site de l’ACA, notre projet dédié à la formation dans l’audiovisuel pour obtenir toutes les informations nécessaires ! Chaque année, plusieurs formations sont organisées en partenariat avec Technifutur, des entreprises du Pôle et l’ACA.</p>
+                <ul>
+                    <li>Développeur web par Becode</li>
+                    <li>Animateur 3D / 2D</li>
+                    <li>Storyboarding</li>
+                    <li>Assistant de production</li>
+                    <li>Salles de montage son/image</li>
+                    <li>Motion Design</li>
+                    <li>Pro Tools</li>
+                </ul>
+
+                <a href="#">Voir le site ACA +</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="entreprises">
+        <h3>Les entreprises</h3>
+
+        <ul>
+            <?php 
+            $args = array(  
+        'post_type' => 'entreprises',
+        'posts_per_page' => 28,
+    );
+
+    $loop = new WP_Query( $args );
+
+    if($loop->have_posts()) : while($loop->have_posts()): $loop->the_post();
+    ?> 
+    <li><?php the_title(); ?></li>
+<?php
+endwhile;
+
+else:
+    ?> <h1>No posts</h1>
+    <?php
+endif;
+     ?>
+        </ul>
+    </section>
 </main>
 <?php get_footer(); ?>
